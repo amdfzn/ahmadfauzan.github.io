9bf1fa3874ae8a70827463039f627632674d5427
Ahmad Fauzan <79957988+amdfzn@users.noreply.github.com>
2022-04-15 00:12:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function onClickID() {
+
+    let h1 = document.getElementById('greet-h1');
+    h1.innerHTML = "Halo!";
+     
+    let h3 = document.getElementById('greet-h3');
+    h3.innerHTML = "Selamat Datang di Website Saya!";
+    
+    let p1 = document.getElementById('about-paragraph');
+    p1.innerHTML = "Saya Ahmad Fauzan, mahasiswa semester 4 Teknik Informatika UIN SUSKA Riau. Lahir dan besar di Pekanbaru hingga saat ini. Memiliki kemampuan yang lebih besar dari badannya, namun sering merasa rendah diri (karena kenyataannya begitu)";
+     
+    let p2= document.getElementById('contact-paragraph');
+    p2.innerHTML = "Untuk informasi lebih lanjut, kontak saya di berbagai media sosial berikut:";
+    
+}
+
+function onClickEN() {
+  
+    let h1 = document.getElementById('greet-h1');
+    h1.innerHTML = "Hello!";
+    
+    let h3 = document.getElementById('greet-h3');
+    h3.innerHTML = "Welcome to My Website!";
+
+    let p1 = document.getElementById('about-paragraph');
+    p1.innerHTML = "My name is Ahmad Fauzan, Informatics Engineering student at State Islamic University Sultan Syarif Kasim Riau, currentlu on level 4. Born and raised in Pekanbaru untill today. Have a big skill, bigger than his body, but sometimes feel insecure (because it's a fact)";
+    
+    let p2= document.getElementById('contact-paragraph');
+    p2.innerHTML = "For more information, you can contact me via social media below:";
+
+}
